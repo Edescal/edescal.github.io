0d520885c95c9d8fdd87a0fb04b4d13ea4a5e35f
E. Escalante <eduardo1582000@gmail.com>
2025-03-19 15:09:29 -0600

    nav

--- static/js/tweenings.js
+++ static/js/tweenings.js
@@ -1,75 +1,59 @@
 class Tween {
-    constructor() {
-        self.time = 0
-        self.tween = null
-        self.pingPong = false;
+    static FPS = 60
+
+    constructor(loop = false, loopPingPong = false, easing = null) {
+        this.time = 0
+        this.tween = null
+        this.loop = loop
+        this.pingPong = loopPingPong;
+        this.easingType = easing
     }
 
-    play(from, to, duration, onUpdate) {
-        
+    play(from, to, duration, onUpdate = null) {
+        let direction = 1
         let stop = false
-        self.time = 0
-        self.tween = setInterval(() => {
-            self.time += 1 / FPS
-            let t = self.time / duration
-            if (t > 1) {
-                t = 1
+        this.time = 0
+        this.tween = setInterval(() => {
+            this.time += (direction / Tween.FPS)
+            this.time = clamp(this.time, 0, duration)
+
+            let t = this.time / duration
+            if (t > 1 || t < 0)
                 stop = true
-            }
+            t = clamp(t, 0, 1)
+
             if (onUpdate) {
-                onUpdate(t)
+                if (this.easingType != null) {
+                    onUpdate(this.easingType( lerp(from, to, t) / to ))
+                } else {
+                    onUpdate(lerp(from, to, t))
+                }
             }
 
             if (stop) {
-                clearInterval(self.tween)
+                stop = false
+                if (this.loop) 
+                    this.time = 0
+                else if (this.pingPong) 
+                    direction *= -1
+                else 
+                    clearInterval(this.tween)
             }
-        }, 1000 / FPS);
-    }
-}
-
-
-let FPS = 60
-let seconds = 8
-
-let element = document.getElementById('alpha')
-function lerp(t) {
-    if (element) {
-        element.style.opacity = t
-        // console.log(t)
-    }
-}
-
-let duration = seconds
-let pingpong = 1
-let tween = setInterval(() => {
-
-    duration -= 1/FPS * pingpong
-    if (duration < 0) {
-        duration = 0
-        pingpong *= -1
-    } else if (duration > seconds) {
-        duration = seconds
-        pingpong *= -1
-    }
-    
-    let t = 1 - (duration / seconds)
-
-    lerp(easeInQuart(t))
-    
-    seconds 
-}, 1/FPS);
-
-
-let ele = document.getElementById('alpha')
-if (ele) {
-    new Tween().play(0,1,3, (t) => {
-        ele.style.opacity = lerp(0, 1, t)
-    })
+        }, 1000 / Tween.FPS);
+    }   
 }
 
 
 function lerp(a, b, t) {
     return a + (b - a) * (t < 0 ? 0 : t > 1 ? 1 : t);
+}
+
+function clamp(x, a, b) {
+    return (x > a ? x : x > b ? b : x)
+}
+
+function easeInOutSine(x) {
+    return -(Math.cos(Math.PI * x) - 1) / 2;
 }
 
 function easeInOutQuad(x) {
@@ -79,3 +63,23 @@ function easeInOutQuad(x) {
 function easeInQuart(x) {
     return x * x * x * x;
 }
+
+
+
+let box = document.getElementById('box')
+if (box) {
+    let tween = new Tween(true, false, easeInOutQuad)
+    tween.play(0, 1, 2, (t) => {
+        box.style.transform = `scale(${lerp(1.1, 0.8, t)}) rotate(${Math.trunc(-t * 270)}deg)`
+        box.style.opacity = lerp(1, 0, t)
+    })
+}
+
+let ele = document.getElementById('alpha')
+if (ele) {
+    new Tween(false, true, easeInOutQuad).play(0.35,1.0,1, (t) => {
+        ele.style.opacity = lerp(0, 1, t)
+    })
+}
+
+
